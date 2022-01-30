6958d25d10581fac4a00fd4867f2b196cba29ca3
yglee <yglee@ksmartpia.com>
2022-01-30 10:18:00 +0900

    AOP 예제 셋팅

---
+++ src/main/java/hello/aop/order/OrderRepository.java
@@ -0,0 +1,22 @@
+package hello.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Repository
+public class OrderRepository {
+
+    public String save(String itemId){
+
+        log.info("[OrderRepository] 싫행");
+        //저장로직
+        if(itemId.equals("ex")){
+            throw new IllegalStateException("예외 발생!");
+        }
+        return "ok";
+
+    }
+
+}
