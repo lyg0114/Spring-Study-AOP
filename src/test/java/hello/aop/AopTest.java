b0add9c2e007f2ad41af8ddeeea472e819d84fa8
yglee <yglee@ksmartpia.com>
2022-01-30 10:58:24 +0900

    스프링 AOP 구현2 - 포인트컷 분리

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,18 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
@@ -18,7 +21,6 @@ public class AopTest {
 
     @Autowired
     OrderRepository orderRepository;
-
 
     @Test
     void aopInfo(){
@@ -37,9 +39,5 @@ public class AopTest {
         Assertions.assertThatThrownBy(()-> orderService.orderItem("ex"))
                 .isInstanceOf(IllegalStateException.class);
     }
-
-
-
-
 
 }
